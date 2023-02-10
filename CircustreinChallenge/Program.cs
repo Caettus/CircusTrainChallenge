45d09605d7606ab6f6c4108ff371d14859892dd5
Caettus <vincentscheffer2@gmail.com>
2023-02-10 22:00:04 +0100

    Animals worden toegevoegd aan List TotalAnimals

--- CircustreinChallenge/Program.cs
+++ CircustreinChallenge/Program.cs
@@ -1,10 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-int QuantityAnimals;
+﻿using ClassLibrary;
+
+Train train;
 
 Console.WriteLine("Welcome to the Animal to Wagon Optimization project!");
 Console.WriteLine("====================================================");
 Console.WriteLine("How many animals do you want");
 
-string input = Console.ReadLine();
-int.TryParse(input, out QuantityAnimals);
 
+string input = Console.ReadLine();
+int.TryParse(input, out int QuantityAnimals);
+
+train = new Train();
+train.GenerateNewAnimals(QuantityAnimals);
+
+Console.WriteLine(train.TotalAnimals.Count);
--- ClassLibrary/Train.cs
+++ ClassLibrary/Train.cs
@@ -2,11 +2,27 @@ namespace ClassLibrary;
 
 public class Train
 {
-    List<Animal> TotalAnimals = new List<Animal>();
+    public List<Animal> TotalAnimals = new List<Animal>();
     List<Wagon> wagons = new List<Wagon>();
 
-    public void GenerateNewAnimals(int Quantity)
+    //public Train(List<Animal> totalAnimals, List<Wagon> wagons)
+    //{
+    //    TotalAnimals = totalAnimals;
+    //    this.wagons = wagons;
+    //} 
+
+    public Train(List<Animal> totalAnimals) 
     {
-        
+        TotalAnimals = totalAnimals;
+    }
+
+    public Train() { }
+
+    public void GenerateNewAnimals(int numberofAnimalsToAdd)
+    {
+        for (int i = 0; i < numberofAnimalsToAdd; i++)
+        {
+            TotalAnimals.Add(new Animal());
+        }
     }
 }
